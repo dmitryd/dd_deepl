bf7e3f536e25dec4dfd011b357365fd39a099179
Dmitry Dulepov <dmitry.dulepov@gmail.com>
2023-07-09 19:07:53 +0300

    [CLEANUP] Improve DataHandlerData usage by using a static variables in the class instaed of a global variable

--- Classes/Hook/DataHandlerHookData.php
+++ Classes/Hook/DataHandlerHookData.php
@@ -1,9 +1,43 @@
 <?php
+
 namespace Dmitryd\DdDeepl\Hook;
+
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2023 Dmitry Dulepov <dmitry.dulepov@gmail.com>
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
 
 use TYPO3\CMS\Core\Site\Entity\SiteLanguage;
 
-class DataHandlerHookData
+/**
+ * This class contains a data structure about the current command that
+ * DataHAndlerTranslationHook needs to save for future use. Since there
+ * can be many DataHandler instances, this data has to be saved per
+ * instance. Such approach saves us from examinining all kind internal
+ * TYPO3 request parameters that can change in any version.
+ *
+ * @author Dmitry Dulepov <support@snowflake.ch>
+ * @internal
+ */
+final class DataHandlerHookData
 {
     protected array $record = [];
 
@@ -16,6 +50,8 @@ class DataHandlerHookData
     protected bool $translationEnabled = false;
 
     /**
+     * Fetches the record.
+     *
      * @return array
      */
     public function getRecord(): array
@@ -24,6 +60,8 @@ class DataHandlerHookData
     }
 
     /**
+     * Sets the record.
+     *
      * @param array $record
      */
     public function setRecord(array $record): void
@@ -32,6 +70,8 @@ class DataHandlerHookData
     }
 
     /**
+     * Fetches the source language.
+     *
      * @return \TYPO3\CMS\Core\Site\Entity\SiteLanguage|null
      */
     public function getSourceLanguage(): ?SiteLanguage
@@ -40,6 +80,8 @@ class DataHandlerHookData
     }
 
     /**
+     * Sets the source language.
+     *
      * @param \TYPO3\CMS\Core\Site\Entity\SiteLanguage|null $sourceLanguage
      */
     public function setSourceLanguage(?SiteLanguage $sourceLanguage): void
@@ -48,6 +90,8 @@ class DataHandlerHookData
     }
 
     /**
+     * Fetches the table name.
+     *
      * @return string
      */
     public function getTableName(): string
@@ -56,6 +100,8 @@ class DataHandlerHookData
     }
 
     /**
+     * Sets the table name.
+     *
      * @param string $tableName
      */
     public function setTableName(string $tableName): void
@@ -64,6 +110,8 @@ class DataHandlerHookData
     }
 
     /**
+     * Gets the target language.
+     *
      * @return \TYPO3\CMS\Core\Site\Entity\SiteLanguage|null
      */
     public function getTargetLanguage(): ?SiteLanguage
@@ -72,6 +120,8 @@ class DataHandlerHookData
     }
 
     /**
+     * Sets the target language.
+     *
      * @param \TYPO3\CMS\Core\Site\Entity\SiteLanguage|null $targetLanguage
      */
     public function setTargetLanguage(?SiteLanguage $targetLanguage): void
@@ -80,6 +130,8 @@ class DataHandlerHookData
     }
 
     /**
+     * Reports if the translation is enabled.
+     *
      * @return bool
      */
     public function isTranslationEnabled(): bool
@@ -88,6 +140,8 @@ class DataHandlerHookData
     }
 
     /**
+     * Sets the enabbe status for translation.
+     *
      * @param bool $translationEnabled
      */
     public function setTranslationEnabled(bool $translationEnabled): void
--- Classes/Hook/DataHandlerTranslationHook.php
+++ Classes/Hook/DataHandlerTranslationHook.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Dmitryd\DdDeepl\Hook;
 
 use DeepL\DeepLException;
@@ -6,7 +7,6 @@ use Dmitryd\DdDeepl\Service\DeeplTranslationService;
 use TYPO3\CMS\Backend\Utility\BackendUtility;
 use TYPO3\CMS\Core\DataHandling\DataHandler;
 use TYPO3\CMS\Core\Exception\SiteNotFoundException;
-use TYPO3\CMS\Core\Site\Entity\SiteLanguage;
 use TYPO3\CMS\Core\Site\SiteFinder;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
@@ -18,14 +18,8 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
  */
 class DataHandlerTranslationHook
 {
-    protected const DATA_NAME = 'TX_DD_DEEPL_DH_DATA';
-
-    public function __construct()
-    {
-        if (!isset($GLOBALS[self::DATA_NAME])) {
-            $GLOBALS[self::DATA_NAME] = [];
-        }
-    }
+    /** @var DataHandlerHookData[] */
+    protected static array $dataHandlerData = [];
 
     /**
      * Hooks into the DataHandler to save data necessary to translating text.
@@ -36,16 +30,15 @@ class DataHandlerTranslationHook
      * @param string $tableName
      * @param int $recordId
      * @param int $languageId
-     * @param \TYPO3\CMS\Core\DataHandling\DataHandler $caller
-     * @param string $fieldName
+     * @param \TYPO3\CMS\Core\DataHandling\DataHandler $dataHandler
      */
-    public function processCmdmap_preProcess(string $command, string $tableName, $recordId, $languageId, DataHandler $dataHandler)
+    public function processCmdmap_preProcess(string $command, string $tableName, mixed $recordId, mixed $languageId, DataHandler $dataHandler): void
     {
         $data = new DataHandlerHookData();
         $data->setTranslationEnabled(false);
 
         if ($command === 'localize' || $command === 'copyToLanguage') {
-            $record = BackendUtility::getRecord($tableName, $recordId);
+            $record = BackendUtility::getRecord($tableName, (int)$recordId);
             try {
                 $languageField = $GLOBALS['TCA'][$tableName]['ctrl']['languageField'];
                 if ($languageField) {
@@ -65,21 +58,36 @@ class DataHandlerTranslationHook
             }
         }
 
-        $GLOBALS[self::DATA_NAME][spl_object_hash($dataHandler)] = $data;
+        self::$dataHandlerData[spl_object_hash($dataHandler)] = $data;
+    }
+
+    /**
+     * Cleans up the datahandler data.
+     *
+     * @param string $command
+     * @param string $table
+     * @param mixed $id
+     * @param mixed $value
+     * @param \TYPO3\CMS\Core\DataHandling\DataHandler $dataHandler
+     */
+    public function processCmdmap_postProcess(/** @noinspection PhpUnusedParameterInspection */string $command, /** @noinspection PhpUnusedParameterInspection */string $table, /** @noinspection PhpUnusedParameterInspection */mixed $id, /** @noinspection PhpUnusedParameterInspection */ mixed $value, DataHandler $dataHandler): void
+    {
+        if (isset(self::$dataHandlerData[spl_object_hash($dataHandler)])) {
+            unset(self::$dataHandlerData[spl_object_hash($dataHandler)]);
+        }
     }
 
     /**
      * Translates the value to a required language.
      *
-     * @param $fieldValue
+     * @param mixed $fieldValue
      * @param array $targetLanguage
      * @param \TYPO3\CMS\Core\DataHandling\DataHandler $dataHandler
      * @param string $fielfdName
-     * @throws \DeepL\DeepLException
      */
-    public function processTranslateTo_copyAction(&$fieldValue, /** @noinspection PhpUnusedParameterInspection */ array $targetLanguage, DataHandler $dataHandler, string $fielfdName)
+    public function processTranslateTo_copyAction(mixed &$fieldValue, /** @noinspection PhpUnusedParameterInspection */ array $targetLanguage, DataHandler $dataHandler, string $fielfdName): void
     {
-        $data = $GLOBALS[self::DATA_NAME][spl_object_hash($dataHandler)] ?? new DataHandlerHookData();
+        $data = self::$dataHandlerData[spl_object_hash($dataHandler)] ?? new DataHandlerHookData();
         if ($data->isTranslationEnabled() && is_string($fieldValue)) {
             try {
                 $fieldValue = GeneralUtility::makeInstance(DeeplTranslationService::class)->translateField(
@@ -90,11 +98,21 @@ class DataHandlerTranslationHook
                     $data->getTargetLanguage()
                 );
             } catch (DeepLException $exception) {
-                $dataHandler->errorLog[] = sprintf(
-                    '[%d] %s',
+                $error = sprintf(
+                    'Error while translating with DeepL: [%d] %s. Stack: %s',
                     $exception->getCode(),
-                    $exception->getMessage()
+                    $exception->getMessage(),
+                    $exception->getTraceAsString()
                 );
+                $dataHandler->log(
+                    $data->getTableName(),
+                    $data->getRecord()['uid'],
+                    2,
+                    $data->getRecord()['pid'],
+                    1,
+                    $error
+                );
+                $dataHandler->errorLog[] = $error;
             }
         }
     }
