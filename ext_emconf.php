06855d884d80644b2576e017fc9823a7a6f62017
Dmitry Dulepov <dmitry.dulepov@gmail.com>
2023-08-27 18:19:54 +0300

    [BUGFIX] Fix typos

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,7 +5,7 @@
  ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = [
-    'title' => 'DeeplL translation for TYPO3 Backend',
+    'title' => 'DeepL translation for TYPO3 Backend',
     'description' => 'Translates pages, content and records using DeeplL',
     'category' => 'be',
     'constraints' => [
@@ -25,5 +25,5 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Dmitry Dulepov',
     'author_email' => 'dmitry.dulepov@gmail.com',
     'author_company' => 'SIA ACCIO',
-    'version' => '1.0.0'
+    'version' => '11.0.0'
 ];
