84d77c3c26029f019f84acb9a330de4b2ebc9439
Dmitry Dulepov <dmitry.dulepov@gmail.com>
2023-08-27 19:21:51 +0300

    [UPDATE] Add information about conflicts

---
+++ ext_emconf.php
@@ -0,0 +1,32 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file.
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'DeepL translation for TYPO3 Backend',
+    'description' => 'Translates pages, content and records using DeeplL',
+    'category' => 'be',
+    'constraints' => [
+        'depends' => [
+            'php' => '8.0.0-8.1.999',
+            'typo3' => '12.4.0-12.4.999',
+            'core' => '',
+            'backend' => '',
+        ],
+        'conflicts' => [
+            'deepltranslate',
+            'wv_deepltranslate',
+        ],
+        'suggests' => [],
+    ],
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearcacheonload' => 1,
+    'author' => 'Dmitry Dulepov',
+    'author_email' => 'dmitry.dulepov@gmail.com',
+    'author_company' => 'SIA ACCIO',
+    'version' => '12.0.1'
+];
