47843a7bfa59bce03ff6b74058f9e22c977da07e
Dmitry Dulepov <dmitry.dulepov@gmail.com>
2023-12-04 17:28:55 +0200

    [UPDATE] Prepare v12.3.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -28,5 +28,5 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Dmitry Dulepov',
     'author_email' => 'dmitry.dulepov@gmail.com',
     'author_company' => 'SIA ACCIO',
-    'version' => '12.3.0'
+    'version' => '12.3.1'
 ];
