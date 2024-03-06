c5cfebae68c597548b843c9d71b24efeaad84113
Dmitry Dulepov <dmitry.dulepov@gmail.com>
2024-03-06 16:26:57 +0300

    [BUGFIX] Fix incompatible console requirements for some installations

--- ext_emconf.php
+++ ext_emconf.php
@@ -28,5 +28,5 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Dmitry Dulepov',
     'author_email' => 'dmitry.dulepov@gmail.com',
     'author_company' => 'SIA ACCIO',
-    'version' => '11.4.0'
+    'version' => '11.4.1'
 ];
