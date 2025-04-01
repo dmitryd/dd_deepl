c17281154481ddd8973efa61d35e977d77ad2631
Dmitry Dulepov <dmitry.dulepov@gmail.com>
2025-04-01 16:39:05 +0300

    [UPDATE] Prepare 13.0.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -28,5 +28,5 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Dmitry Dulepov',
     'author_email' => 'dmitry.dulepov@gmail.com',
     'author_company' => '',
-    'version' => '13.0.0'
+    'version' => '13.0.1'
 ];
