b862b7810dcff937cd72e33c43fc1803f2d140fc
Dmitry Dulepov <dmitry.dulepov@gmail.com>
2025-05-28 16:15:05 +0300

    [UPDATE] Pewpare 13.1.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -28,5 +28,5 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Dmitry Dulepov',
     'author_email' => 'dmitry.dulepov@gmail.com',
     'author_company' => '',
-    'version' => '13.1.0'
+    'version' => '13.1.1'
 ];
