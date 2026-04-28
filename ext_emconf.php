001c086eb94616ebe3bbc22a717d7836cb5ac774
Dmitry Dulepov <dmitry.dulepov@gmail.com>
2026-04-28 17:20:32 +0300

    [BUGFIX] #64: fix extension version in ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -28,5 +28,5 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Dmitry Dulepov',
     'author_email' => 'dmitry.dulepov@gmail.com',
     'author_company' => '',
-    'version' => '14.0.0',
+    'version' => '14.0.2',
 ];
